dd164685d53a389caefa6454f45e0d0e47d3fd8a
[Sona] <[sona16061999@gmail.com]>
2021-06-30 14:34:58 +0530

    Start

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,7 +6,14 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake & Ladder Simulator ");
+            int position = 0; //local variable
+            int player = 1;
+              // creating an object
+            Random random = new Random();
+            // calling the Next() Method for generating random values
+            int dieInput = random.Next(1, 7);
+            Console.WriteLine("Snake and Laddergame played by a player" + " " + player + " " + "at start position" + " " + position);
         }
+        
     }
 }
