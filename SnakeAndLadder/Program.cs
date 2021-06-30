c1cfb9d34179282a9ee7ba5e2bfdf0d7d6740bc6
[Sona] <[sona16061999@gmail.com]>
2021-06-30 15:09:10 +0530

    Reaches the postion

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,30 +7,48 @@ namespace SnakeAndLadder
         public const int LADDER_POS = 2;
         public const int SNAKE_POS = 1;
         public const int NO_PLAY = 0;
-    
-         public static void Main(string[] args)
+        public const int USER_DES = 100;
+
+        public static void Main(string[] args)
         {
             int position = 0; //local variable
             int player = 1;
             Random random = new Random();
-            // calling the Next() Method for generating random values
-            int dieInput = random.Next(1, 7);
-            int choice = random.Next(1, 3);
-            // Selection statement
-
-            switch (choice)
+            
+            for (position = 0; position <= USER_DES; position++)
             {
-                case LADDER_POS:
-                    position += dieInput;
+                // calling the Next() Method for generating random values
+                int dieInput = random.Next(1, 7);
+                int choice = random.Next(1, 3);
+                // Selection statement
+                switch (choice)
+                {
+                    case LADDER_POS:
+                        position += dieInput;
+                        break;
+                    case SNAKE_POS:
+                        position -= dieInput;
+                        break;
+                    case NO_PLAY:
+                        position += 0;
+                        break;
+                }
+                if(position<0)
+                {
+                    position = 0;
+                }
+                else if(position==USER_DES)
+                {
                     break;
-                case SNAKE_POS:
-                    position -= dieInput;
-                    break;
-                case NO_PLAY:
-                    position += 0;
-                    break;
+                }
+                else
+                {
+                    position++;
+                }
+
+
             }
-            Console.WriteLine("The player" + " " + player + "rolles the die and get " + " " + position);
+            Console.WriteLine("The player" + " " + player + " " + "won the game by reaching" + " " + position);
 
         }
         
